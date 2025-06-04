d892554ab111a517a6e9419768c79bfafd83a95a
codewithsanju-roy <sanjuroy806@gmail.com>
2025-06-04 12:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
-//complete this code
 class Person {
-	constructor(name, age){
-		this._name = name;
-			this._age = age;
-	}
-	get name(){
-		return this._name;
-	}
-	set age(){
-		return this._age;
-		
-	}
-}
+      constructor(name, age) {
+        this._name = name;
+        this._age = age;
+      }
 
-class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`)
-	}
-}
+      get name() {
+        return this._name;
+      }
 
-class Teacher extends Person {
-	tech(){
-		console.log(`${this.name} is teching`)
-	}
-}
+      get age() {
+        return this._age;
+      }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+      set age(newAge) {
+        this._age = newAge;
+      }
+    }
+
+    class Student extends Person {
+      study() {
+        logMessage(${this.name} is studying);
+      }
+    }
+
+    class Teacher extends Person {
+      teach() {
+        logMessage(${this.name} is teaching);
+      }
+    }
+
+    // Helper function to log to the page
+    function logMessage(message) {
+      const output = document.getElementById('output');
+      const p = document.createElement('p');
+      p.textContent = message;
+      output.appendChild(p);
+      console.log(message); // also log to the console
+    }
+
+    // Do not change the code below this line
+    window.Person = Person;
+    window.Student = Student;
+    window.Teacher = Teacher;
+
+    // Example usage to show it works
+    const person = new Person("John", 25);
+    logMessage(Name: ${person.name});
+    person.age = 30;
+    logMessage(Updated Age: ${person.age});
+
+    const student = new Student("Alice", 22);
+    student.study();
+
+    const teacher = new Teacher("Bob", 40);
+    teacher.teach();
